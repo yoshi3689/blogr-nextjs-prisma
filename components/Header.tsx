abfcfff25ec55a3cd6326550431d7d29282790a5
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2023-08-25 15:02:16 -0700

    add frontend logic for the login system

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { signOut, useSession } from "next-auth/react";
 
 const Header: React.FC = () => {
   const router = useRouter();
   const isActive: (pathname: string) => boolean = (pathname) =>
     router.pathname === pathname;
 
+  const { data: session, status } = useSession();
+
   let left = (
     <div className="left">
-      {isActive("/") &&
-        <Link href="/" >Feed</Link>
-      }
+      {isActive("/") && <Link href="/">Feed</Link>}
       <style jsx>{`
         .bold {
           font-weight: bold;
@@ -19,7 +20,7 @@ const Header: React.FC = () => {
 
         a {
           text-decoration: none;
-          color: #000;
+          color: var(--geist-foreground);
           display: inline-block;
         }
 
@@ -35,6 +36,142 @@ const Header: React.FC = () => {
   );
 
   let right = null;
+
+  if (status === "loading") {
+    left = (
+      <div className="left">
+        {isActive("/") && <Link href="/">Feed</Link>}
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+
+          a {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          .left a[data-active="true"] {
+            color: gray;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    );
+    right = (
+      <div className="right">
+        <p>Validating session ...</p>
+        <style jsx>{`
+          .right {
+            margin-left: auto;
+          }
+        `}</style>
+      </div>
+    );
+  }
+
+  if (!session) {
+    right = (
+      <div className="right">
+        {isActive("/signup") && <Link href="/api/auth/signin">Log in</Link>}
+        <style jsx>{`
+          a {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right a {
+            border: 1px solid var(--geist-foreground);
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+
+  if (session) {
+    left = (
+      <div className="left">
+        {isActive("/") && <Link href="/">Feed</Link>}
+        {isActive("/drafts") && <Link href="/drafts">My drafts</Link>}
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+
+          a {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          .left a[data-active="true"] {
+            color: gray;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    );
+    right = (
+      <div className="right">
+        <p>
+          {session.user.name} ({session.user.email})
+        </p>
+        <Link href="/create">
+          <button>New post</button>
+        </Link>
+        <button onClick={() => signOut()}>Log out</button>
+        <style jsx>{`
+          a {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          p {
+            display: inline-block;
+            font-size: 13px;
+            padding-right: 1rem;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right a {
+            border: 1px solid var(--geist-foreground);
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+
+          button {
+            border: none;
+          }
+        `}</style>
+      </div>
+    );
+  }
 
   return (
     <nav>
