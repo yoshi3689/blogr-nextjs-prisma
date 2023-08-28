4907cced6b49dc266a76be522395b47f2dfa975a
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2023-08-27 18:30:35 -0700

    add google as another oauth signin provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth, {NextAuthOptions} from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import prisma from "../../../lib/prisma";
 
 const authOptions: NextAuthOptions = {
@@ -9,12 +10,22 @@ const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      // to force create refresh token by google oauth
+      authorization: {
+        params: {
+          prompt: "consent",
+        },
+      },
+    }),
   ],
   adapter: PrismaAdapter(prisma),
   callbacks: {
     async jwt({ token }) {
-      token.userRole = "admin"
-      return token
+      token.userRole = "admin";
+      return token;
     },
   },
   // secret: process.env.SECRET,
