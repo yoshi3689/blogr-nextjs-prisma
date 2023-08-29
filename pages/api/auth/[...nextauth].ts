e5d5f1a73b63fbfdeefa276fb00a555dea588382
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2023-08-29 14:43:19 -0700

    add a test log in method

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,25 +2,54 @@ import NextAuth, {NextAuthOptions} from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "../../../lib/prisma";
 
-const authOptions: NextAuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      // to force create refresh token by google oauth
-      authorization: {
-        params: {
-          prompt: "consent",
-        },
+const testProvider = [
+  CredentialsProvider({
+  name: "Credentials",
+  credentials: {
+    username: {
+      label: "Username",
+      type: "text",
+      placeholder: "jsmith",
+    },
+    password: { label: "Password", type: "password" },
+  },
+  //@ts-ignore
+  async authorize() {
+    return {
+      id: 1,
+      name: "J Smith",
+      email: "jsmith@example.com",
+      image: "https://i.pravatar.cc/150?u=jsmith@example.com",
+    }
+  },
+})
+]
+
+const oAuthProviders = [
+  GitHubProvider({
+    clientId: process.env.GITHUB_ID,
+    clientSecret: process.env.GITHUB_SECRET,
+  }),
+  GoogleProvider({
+    clientId: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    // to force create refresh token by google oauth
+    authorization: {
+      params: {
+        prompt: "consent",
       },
-    }),
-  ],
+    },
+  })
+];
+
+const authOptions: NextAuthOptions = {
+  providers: process.env.VERCEL_ENV === "preview" || process.env.VERCEL_ENV === "development"
+    ? testProvider
+    : oAuthProviders
+  ,
   adapter: PrismaAdapter(prisma),
   callbacks: {
     async jwt({ token }) {
